d1662d6addc1f23bdb0849d9ee6f0b8e87b36a9a
Török Róbert <robert.torok@gmail.com>
2020-05-01 13:42:28 +0200

    fixes in project csillagvarta-app

--- app/csillagvarta-app.pro
+++ app/csillagvarta-app.pro
@@ -5,16 +5,10 @@ COMPONENT_NAME=csillagvarta
 include($${BUILD_ROOT}/sdk/build/qmake/application.pri)
 
 SOURCES += \
-    main.cc \
-    mainwindow.cc
-    
-lupdate_only {
-SOURCES +=
-}
+    main.cc
 
 HEADERS += \
     init.h \
-    mainwindow.h \
     setup.h \
     defs.h
 
@@ -38,8 +32,3 @@ android: {
 }
 
 QMAKE_EXTRA_TARGETS += $$object_dep_on_module_header(main)
-
-#include($${BUILD_ROOT}/sdk/build/qmake/qm-compile.pri)
-
-FORMS += \
-    mainwindow.ui
--- app/init.h
+++ app/init.h
@@ -3,26 +3,16 @@
 #define __SYMBOID_CSILLAGVARTA_APP_INIT_H__
 
 #include "csillagvarta/app/defs.h"
-#define APP_QML
-#ifdef APP_QML
-#   include "sdk/arch/appqml.h"
-template <class _App>
-using app_base = arh::app_qml<_App>;
-#else
-#   include "sdk/arch/appqtw.h"
-#   include "csillagvarta/app/mainwindow.h"
-template <class _App>
-using app_base = arh::app_qtw<_App,MainWindow>;
-#endif
+#include "sdk/arch/appqml.h"
 #include "astro/uicontrols-qt/init.h"
 #include "sdk/dox-qt/init.h"
 
-struct app_csillagvarta : app_base<app_csillagvarta>
+struct app_csillagvarta : arh::app_qml<app_csillagvarta>
 {
     APP_OBJECT(csillagvarta)
 
     app_csillagvarta(int* _argc, char*** _argv)
-        : app_base<app_csillagvarta>(_argc, _argv)
+        : arh::app_qml<app_csillagvarta>(_argc, _argv)
     {
     }
 

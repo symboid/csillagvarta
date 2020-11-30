071602cf8041ea486fa760cbaef71e53950c4ed5
Török Róbert <robert.torok@gmail.com>
2020-11-30 21:16:59 +0100

    Update init.cc

--- app/init.cc
+++ app/init.cc
@@ -10,22 +10,22 @@ app_csillagvarta::app_csillagvarta(int* _argc, char*** _argv)
     load_translator();
 
     arh::main_object<QHoraConfig> horaConfig;
-    horaConfig->mPlanets.push_back(hor::planet(hor::planet::sun, new OrbisConfig(horaConfig->aspects(), 0)));
-    horaConfig->mPlanets.push_back(hor::planet(hor::planet::moon, new OrbisConfig(horaConfig->aspects(), 1)));
-    horaConfig->mPlanets.push_back(hor::planet(hor::planet::mercury, new OrbisConfig(horaConfig->aspects(), 2)));
-    horaConfig->mPlanets.push_back(hor::planet(hor::planet::venus, new OrbisConfig(horaConfig->aspects(), 3)));
-    horaConfig->mPlanets.push_back(hor::planet(hor::planet::mars, new OrbisConfig(horaConfig->aspects(), 4)));
-    horaConfig->mPlanets.push_back(hor::planet(hor::planet::jupiter, new OrbisConfig(horaConfig->aspects(), 5)));
-    horaConfig->mPlanets.push_back(hor::planet(hor::planet::saturn, new OrbisConfig(horaConfig->aspects(), 6)));
+    horaConfig->mPlanets.push_back(hor::planet(hor::planet::sun, new OrbisConfig(horaConfig->aspects(), &QOrbisConfigNode::sunNode)));
+    horaConfig->mPlanets.push_back(hor::planet(hor::planet::moon, new OrbisConfig(horaConfig->aspects(), &QOrbisConfigNode::monNode)));
+    horaConfig->mPlanets.push_back(hor::planet(hor::planet::mercury, new OrbisConfig(horaConfig->aspects(), &QOrbisConfigNode::merNode)));
+    horaConfig->mPlanets.push_back(hor::planet(hor::planet::venus, new OrbisConfig(horaConfig->aspects(), &QOrbisConfigNode::venNode)));
+    horaConfig->mPlanets.push_back(hor::planet(hor::planet::mars, new OrbisConfig(horaConfig->aspects(), &QOrbisConfigNode::marNode)));
+    horaConfig->mPlanets.push_back(hor::planet(hor::planet::jupiter, new OrbisConfig(horaConfig->aspects(), &QOrbisConfigNode::jupNode)));
+    horaConfig->mPlanets.push_back(hor::planet(hor::planet::saturn, new OrbisConfig(horaConfig->aspects(), &QOrbisConfigNode::satNode)));
 
-    horaConfig->mPlanets.push_back(hor::planet(hor::planet::uranus, new OrbisConfig(horaConfig->aspects(), 7)));
-    horaConfig->mPlanets.push_back(hor::planet(hor::planet::neptune, new OrbisConfig(horaConfig->aspects(), 8)));
-    horaConfig->mPlanets.push_back(hor::planet(hor::planet::pluto, new OrbisConfig(horaConfig->aspects(), 9)));
+    horaConfig->mPlanets.push_back(hor::planet(hor::planet::uranus, new OrbisConfig(horaConfig->aspects(), &QOrbisConfigNode::uraNode)));
+    horaConfig->mPlanets.push_back(hor::planet(hor::planet::neptune, new OrbisConfig(horaConfig->aspects(), &QOrbisConfigNode::nepNode)));
+    horaConfig->mPlanets.push_back(hor::planet(hor::planet::pluto, new OrbisConfig(horaConfig->aspects(), &QOrbisConfigNode::pluNode)));
 
-    horaConfig->mPlanets.push_back(hor::planet(hor::planet::dragon_head, new OrbisConfig(horaConfig->aspects(), 12)));
+    horaConfig->mPlanets.push_back(hor::planet(hor::planet::dragon_head, new OrbisConfig(horaConfig->aspects(), &QOrbisConfigNode::nodNode)));
+    horaConfig->mPlanets.push_back(hor::planet(hor::planet::lilith, new OrbisConfig(horaConfig->aspects(), &QOrbisConfigNode::lilNode)));
+
 //    horaConfig->mPlanets.push_back(hor::planet(hor::planet::chiron, new hor::simple_orbis_config(0.5)));
-    horaConfig->mPlanets.push_back(hor::planet(hor::planet::lilith, new hor::simple_orbis_config(1.5)));
-
 }
 
 app_csillagvarta::~app_csillagvarta()

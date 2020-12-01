38b8d3bc1dc6e41f1af57f7ec5d27d9f93792116
Török Róbert <torok.robert@symboid.com>
2020-12-01 14:01:28 +0100

    Update init.cc

--- app/init.cc
+++ app/init.cc
@@ -8,24 +8,6 @@ app_csillagvarta::app_csillagvarta(int* _argc, char*** _argv)
 {
     Q_INIT_RESOURCE(csillagvarta_app);
     load_translator();
-
-    arh::main_object<QHoraConfig> horaConfig;
-    horaConfig->mPlanets.push_back(hor::planet(hor::planet::sun, new OrbisConfig(horaConfig->aspects(), &QOrbisConfigNode::sunNode)));
-    horaConfig->mPlanets.push_back(hor::planet(hor::planet::moon, new OrbisConfig(horaConfig->aspects(), &QOrbisConfigNode::monNode)));
-    horaConfig->mPlanets.push_back(hor::planet(hor::planet::mercury, new OrbisConfig(horaConfig->aspects(), &QOrbisConfigNode::merNode)));
-    horaConfig->mPlanets.push_back(hor::planet(hor::planet::venus, new OrbisConfig(horaConfig->aspects(), &QOrbisConfigNode::venNode)));
-    horaConfig->mPlanets.push_back(hor::planet(hor::planet::mars, new OrbisConfig(horaConfig->aspects(), &QOrbisConfigNode::marNode)));
-    horaConfig->mPlanets.push_back(hor::planet(hor::planet::jupiter, new OrbisConfig(horaConfig->aspects(), &QOrbisConfigNode::jupNode)));
-    horaConfig->mPlanets.push_back(hor::planet(hor::planet::saturn, new OrbisConfig(horaConfig->aspects(), &QOrbisConfigNode::satNode)));
-
-    horaConfig->mPlanets.push_back(hor::planet(hor::planet::uranus, new OrbisConfig(horaConfig->aspects(), &QOrbisConfigNode::uraNode)));
-    horaConfig->mPlanets.push_back(hor::planet(hor::planet::neptune, new OrbisConfig(horaConfig->aspects(), &QOrbisConfigNode::nepNode)));
-    horaConfig->mPlanets.push_back(hor::planet(hor::planet::pluto, new OrbisConfig(horaConfig->aspects(), &QOrbisConfigNode::pluNode)));
-
-    horaConfig->mPlanets.push_back(hor::planet(hor::planet::dragon_head, new OrbisConfig(horaConfig->aspects(), &QOrbisConfigNode::nodNode)));
-    horaConfig->mPlanets.push_back(hor::planet(hor::planet::lilith, new OrbisConfig(horaConfig->aspects(), &QOrbisConfigNode::lilNode)));
-
-//    horaConfig->mPlanets.push_back(hor::planet(hor::planet::chiron, new hor::simple_orbis_config(0.5)));
 }
 
 app_csillagvarta::~app_csillagvarta()

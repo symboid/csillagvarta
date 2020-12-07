085b832631517da98f12668ddc9e7f31d711d32c
Török Róbert <torok.robert@symboid.com>
2020-12-07 16:22:30 +0100

    fix

--- app/csillagvarta-app_hu.ts
+++ app/csillagvarta-app_hu.ts
@@ -69,6 +69,10 @@
         <source>Failed to save horoscope of &apos;%1&apos;!</source>
         <translation type="unfinished"></translation>
     </message>
+    <message>
+        <source>Horoscope of &apos;%1&apos; saved.</source>
+        <translation type="unfinished"></translation>
+    </message>
 </context>
 <context>
     <name>SettingsScreen</name>

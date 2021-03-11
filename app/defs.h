bdd302cb10174d6fb5e6090f45e123ebbc77eb43
Török Róbert <torok.robert@symboid.com>
2021-03-11 12:57:23 +0100

    Update defs.h

--- app/defs.h
+++ app/defs.h
@@ -2,6 +2,6 @@
 #ifndef __SYMBOID_CSILLAGVARTA_APP_DEFS_H__
 #define __SYMBOID_CSILLAGVARTA_APP_DEFS_H__
 
-#include "sdk/defs.h"
+#include "sdk/arch/defs.h"
 
 #endif // __SYMBOID_CSILLAGVARTA_APP_DEFS_H__
